9c8f32a0ad3a793b497c0085d25dba87815c0ac2
TheReven <jan.reven@gmail.com>
2025-08-04 21:13:38 +0200

    za deployment narjen build in prmaknn v backend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
-require('dotenv').config()
-const db = require('./db.js')
+require('dotenv').config();
+const path = require('path');
+const db = require('./db.js');
 const cors = require('cors');
+
 const app = express();
 
-const oglasiRoutes = require('./routes/oglasi.js')
-const usersRoutes = require('./routes/users.js')
+const oglasiRoutes = require('./routes/oglasi.js');
+const usersRoutes = require('./routes/users.js');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', usersRoutes)
-app.use('/oglasi', oglasiRoutes)
+app.use('/users', usersRoutes);
+app.use('/oglasi', oglasiRoutes);
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
